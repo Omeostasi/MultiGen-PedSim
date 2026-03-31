736e8037db7cac5a161e82ec1aefb3bfa2031487
Giovanni Vincenzo Cavallo <202402143@post.au.dk>
2026-03-31 14:53:34 +0200

    deleting stuff that shouldn't be there

--- benchmarking_haplo/combine_files.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-args <- commandArgs(trailingOnly = TRUE)
-
-data_list <- lapply(args, readRDS)
-
-combined_df <- bind_rows(data_list)
-
-saveRDS(combined_df, "results/combined_results.rds")
